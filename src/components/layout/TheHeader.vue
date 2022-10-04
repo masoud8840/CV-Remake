fe18f02187658bda664e75310a73bea212329e93
masoud8840 <masoudgharedaghi@yahoo.com>
2022-10-04 11:35:26 +0330

    finish header

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="h-[250px] bg-bg-dark flex items-center">
+    <div class="container mx-auto flex items-center justify-between">
+      <div class="name">
+        <span
+          class="header-subtitle leading-[18px] text-text-light font-medium text-xs"
+        >
+          RESUME
+        </span>
+        <h2 class="font-semibold text-4xl leading-[54px] text-text-dark">
+          Masoud Gharedaghi
+        </h2>
+        <span
+          class="header-subtitle leading-[18px] text-text-light text-xs font-medium"
+        >
+          WEB DESIGNER & DEVELOPER
+        </span>
+      </div>
+      <div class="social grid gap-[10px]">
+        <div class="social-item flex gap-[10px] items-center">
+          <img src="/Logo_Telegram.svg" alt="telegram" />
+          <span class="leading-[18px] font-medium text-xs text-text-semidark"
+            >@weber_man</span
+          >
+        </div>
+        <div class="social-item flex gap-[10px] items-center">
+          <img src="/Logo_Instagram.svg" alt="telegram" />
+          <span class="leading-[18px] font-medium text-xs text-text-semidark"
+            >gharedaghi.ce</span
+          >
+        </div>
+        <div class="social-item flex gap-[10px] items-center">
+          <img src="/Logo_Github.svg" alt="telegram" />
+          <span class="leading-[18px] font-medium text-xs text-text-semidark"
+            >masoud8840</span
+          >
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-light': "#7e7e7e",
+        'text-dark': '#333333',
+        'text-semidark':'#565656',
+        'bg-light': '#e9e9e9',
+        'bg-dark': '#d9d9d9',
+      }
+    },
   },
   plugins: [],
 }
