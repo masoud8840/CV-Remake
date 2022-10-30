1cde46a655eb111bb88fd83a4c382574b815b148
masoud8840 <masoudgharedaghi@yahoo.com>
2022-10-30 20:40:07 +0330

    added socials link

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -7,19 +7,40 @@
         <span class="header-subtitle"> WEB DESIGNER & DEVELOPER </span>
       </div>
       <div class="social">
-        <div class="social-item">
-          <img src="/Logo_Telegram.svg" alt="telegram" />
-          <span class="social-caption">@weber_man</span>
-        </div>
-        <div class="social-item">
-          <img src="/Logo_Instagram.svg" alt="instagram" />
-          <span class="social-caption">gharedaghi.ce</span>
-        </div>
-        <div class="social-item">
-          <img src="/Logo_Github.svg" alt="github" />
-          <span class="social-caption">masoud8840</span>
-        </div>
+        <a
+          target="_blank"
+          :href="social.link"
+          class="social-item"
+          v-for="(social, index) in socials"
+          :key="index"
+        >
+          <img :src="`/Logo_${social.name}.svg`" :alt="social.name" />
+          <span class="social-caption">{{ social.id }}</span>
+        </a>
       </div>
     </div>
   </header>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const socials = ref([
+  {
+    name: "Telegram",
+    id: "@weber_man",
+    link: "https://telegram.me/weber_man",
+  },
+  {
+    name: "Instagram",
+    id: "gharedaghi.ce",
+    link: "https://instagram.com/gharedaghi.ce",
+  },
+  {
+    name: "Github",
+    id: "masoud8840",
+    link: "https://github.com/masoud8840",
+  },
+]);
+
+// https://telegram.me/weber_man
+</script>
